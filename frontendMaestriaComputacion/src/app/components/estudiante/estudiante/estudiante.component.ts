d09412f48569694a6b11f2b1d5cbc77669ddf475
= <=>
2023-02-09 11:58:22 -0500

    listar preliminar, realiza get all a estudiantes, falta cuadrar detaller pero es funcinal, tambien se corrige el merge del back, se insertan scripts para bootstrap y se installa sweetalert2

--- frontendMaestriaComputacion/src/app/components/estudiante/estudiante/estudiante.component.ts
+++ frontendMaestriaComputacion/src/app/components/estudiante/estudiante/estudiante.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { EstudianteService } from 'src/app/services/estudiante.service';
+import { Estudiante } from '../modelos/Estudiante';
+import { Telefono } from '../modelos/Telefono';
+import { Direccion } from '../modelos/Direccion';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-estudiante',
@@ -6,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./estudiante.component.css']
 })
 export class EstudianteComponent {
+  estudiantes!: Estudiante[];
 
+  constructor(private servicio: EstudianteService){
+
+  }
+
+  ngOnInit(): void{
+    this.servicio.getAllEstudiantes().subscribe(
+      (result) => {
+        this.estudiantes = result;
+      }
+    );
+  }
 }
