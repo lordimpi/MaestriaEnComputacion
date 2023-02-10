1e411599b15264a2f2d1d0d328b2ecbc4feea498
hecothebest <57530331+hecothebest@users.noreply.github.com>
2023-02-10 15:18:30 -0500

    LISTAR DOCENTE

---
+++ frontendMaestriaComputacion/src/app/models/docente.ts
@@ -0,0 +1,11 @@
+export class Docente {
+  id!:number;
+  noId!:string;
+  tipoIdentificacion!:string;
+  nombres!:string;
+  apellidos!:string;
+  universidad!:string;
+  tipoDocente!:string;
+  salario!:number;
+
+}
--- frontendMaestriaComputacion/src/app/services/docente.service.ts
+++ frontendMaestriaComputacion/src/app/services/docente.service.ts
@@ -1,9 +1,19 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Docente } from '../models/docente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocenteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8081/api/docentes';
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  getDocentes():Observable<Docente[]>{
+      return this.http.get<Docente[]>(this.urlEndPoint)
+  }
 }
